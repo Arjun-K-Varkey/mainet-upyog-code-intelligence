82f97d9ddb2a0635b7d1c44247bd5d0e482dd090
Arjun-K-Varkey <arjunkvarkey@gmail.com>
2026-09-19 19:41:36 +0530

    feat: export trace engine

--- src/aca_graph/__init__.py
+++ src/aca_graph/__init__.py
@@ -2,5 +2,19 @@
 
 from .model import Edge, Graph, Node
 from .builder import build_from_ingestion
+from .trace import (
+    TRACE_SCHEMA_VERSION,
+    TRACE_STATES,
+    TraceEngine,
+    TracePath,
+    TraceRequest,
+    TraceResult,
+    TraceValidationError,
+)
 
-__all__ = ["Node", "Edge", "Graph", "build_from_ingestion"]
+__all__ = [
+    "Node", "Edge", "Graph", "build_from_ingestion",
+    "TRACE_SCHEMA_VERSION", "TRACE_STATES",
+    "TraceEngine", "TracePath", "TraceRequest", "TraceResult",
+    "TraceValidationError",
+]
