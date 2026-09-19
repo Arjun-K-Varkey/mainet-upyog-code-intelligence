feaf601ae582c65a9012f2eb6e9b6294d2e35a2a
Arjun-K-Varkey <arjunkvarkey@gmail.com>
2026-09-19 20:35:41 +0530

    fix: export canonical trace models

--- src/aca_graph/__init__.py
+++ src/aca_graph/__init__.py
@@ -5,16 +5,20 @@ from .builder import build_from_ingestion
 from .trace import (
     TRACE_SCHEMA_VERSION,
     TRACE_STATES,
+    Trace,
+    TraceBoundary,
     TraceEngine,
-    TracePath,
     TraceRequest,
-    TraceResult,
+    TraceStep,
+    CandidatePath,
     TraceValidationError,
+    validate_trace_dict,
 )
 
 __all__ = [
     "Node", "Edge", "Graph", "build_from_ingestion",
     "TRACE_SCHEMA_VERSION", "TRACE_STATES",
-    "TraceEngine", "TracePath", "TraceRequest", "TraceResult",
-    "TraceValidationError",
+    "Trace", "TraceBoundary", "TraceEngine", "TraceRequest",
+    "TraceStep", "CandidatePath", "TraceValidationError",
+    "validate_trace_dict",
 ]
